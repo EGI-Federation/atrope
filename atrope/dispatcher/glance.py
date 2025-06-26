5d9b9b29089c3da82ae37a1b915ec8b4472087d6
Enol Fernandez <enol.fernandez@egi.eu>
2025-06-26 13:43:34 +0100

    Moving from python-glanceclient to openstacksdk
    python-glanceclient does not work at all sites (SCAI)

--- atrope/dispatcher/glance.py
+++ atrope/dispatcher/glance.py
@@ -15,10 +15,10 @@
 import json
 
 import glanceclient.client
+from openstack import connection
 import yaml
 from glanceclient import exc as glance_exc
 from keystoneauth1 import loading
-from keystoneclient.v3 import client as ks_client_v3
 from oslo_config import cfg
 from oslo_log import log
 
@@ -49,6 +49,7 @@ CONF.register_opts(opts, group=CFG_GROUP)
 
 loading.register_auth_conf_options(CONF, CFG_GROUP)
 loading.register_session_conf_options(CONF, CFG_GROUP)
+loading.register_adapter_conf_options(CONF, CFG_GROUP)
 
 opts = (
     loading.get_auth_common_conf_options()
@@ -86,26 +87,10 @@ class Dispatcher(base.BaseDispatcher):
     """
 
     def __init__(self):
-        self.client = self._get_glance_client()
-        self.ks_client = self._get_ks_client()
+        self.client = self._get_openstack_client()
         self.vo_map = self._read_vo_map()
 
-        # Format is not defined in the spec. What is format? Maybe it is the
-        # container format? Or is it the image format? Try to do some ugly
-        # magic and infer what is this for...
-        # This makes me sad :-(
-        LOG.debug(
-            "The image spec is broken and I will try to guess what "
-            "the container and the image format are. I cannot "
-            "promise anything."
-        )
-
-    def _get_ks_client(self):
-        auth_plugin = loading.load_auth_from_conf_options(CONF, CFG_GROUP)
-        sess = loading.load_session_from_conf_options(CONF, CFG_GROUP, auth=auth_plugin)
-        return ks_client_v3.Client(session=sess)
-
-    def _get_glance_client(self, project_id=None):
+    def _get_openstack_client(self, project_id=None):
         if project_id:
             auth_plugin = loading.load_auth_from_conf_options(
                 CONF, CFG_GROUP, project_id=project_id
@@ -116,7 +101,11 @@ class Dispatcher(base.BaseDispatcher):
         session = loading.load_session_from_conf_options(
             CONF, CFG_GROUP, auth=auth_plugin
         )
-        return glanceclient.client.Client(2, session=session)
+        conn = connection.Connection(
+            session=session,
+            oslo_conf=CONF,
+        )
+        return conn
 
     def _read_vo_map(self):
         try:
@@ -138,9 +127,9 @@ class Dispatcher(base.BaseDispatcher):
         metadata = {
             "name": image_name,
             "tags": [CONF.glance.tag],
+            "container_format": "bare",
             "architecture": image.arch,
             "disk_format": None,
-            "container_format": "bare",
             "os_distro": image.osname.lower(),
             "os_version": image.osversion,
             "visibility": "public" if is_public else "private",
@@ -163,14 +152,12 @@ class Dispatcher(base.BaseDispatcher):
                 raise exception.MetadataOverwriteNotSupported(key=k)
             metadata[k] = v
 
-        kwargs = {
-            "filters": {
-                "tag": [CONF.glance.tag],
-                "appdb_id": image.identifier,
-            }
+        query = {
+            "tag": [CONF.glance.tag],
+            "appdb_id": image.identifier,
         }
         # TODO(aloga): what if we have several images here?
-        images = list(self.client.images.list(**kwargs))
+        images = list(self.client.image.images(tag=CONF.glance.tag))
         if len(images) > 1:
             images = [img.id for img in images]
             LOG.error(
@@ -185,14 +172,14 @@ class Dispatcher(base.BaseDispatcher):
         except IndexError:
             glance_image = None
         else:
-            if glance_image.image_hash != image.hash:
+            if glance_image.properties.get("image_hash", "") != image.hash:
                 LOG.warning(
                     "Image '%s' is '%s' in glance but checksums"
                     "are different, deleting it and reuploading.",
                     image.identifier,
                     glance_image.id,
                 )
-                self.client.images.delete(glance_image.id)
+                self.client.image.delete(glance_image)
                 glance_image = None
 
         metadata["disk_format"], image_fd = image.convert(CONF.glance.formats)
@@ -215,11 +202,11 @@ class Dispatcher(base.BaseDispatcher):
 
         if not glance_image:
             LOG.debug("Creating image '%s'.", image.identifier)
-            glance_image = self.client.images.create(**metadata)
+            glance_image = self.client.image.create_image(**metadata, allow_duplicates=True)
 
         if glance_image.status == "queued":
             LOG.debug("Uploading image '%s'.", image.identifier)
-            self._upload(glance_image.id, image_fd)
+            glance_image.upload(self.client.image, data=image_fd)
 
         if glance_image.status == "active":
             LOG.info(
@@ -269,20 +256,22 @@ class Dispatcher(base.BaseDispatcher):
         This method will remove images that were not set to be dispatched
         (i.e. that are not included in the list) that are present in Glance.
         """
-        kwargs = {
+        query = {
             "filters": {
                 "tag": [CONF.glance.tag],
                 "image_list": image_list.name,
             }
         }
         valid_images = [i.identifier for i in image_list.get_valid_subscribed_images()]
-        for image in self.client.images.list(**kwargs):
-            appdb_id = image.get("appdb_id", "")
+        for image in self.client.image.images(tag=CONF.glance.tag):
+            if image.properties.get("image_list", "") != image_list.name:
+                continue
+            appdb_id = image.properties.get("appdb_id", "")
             if appdb_id not in valid_images:
                 LOG.warning(
                     "Glance image '%s' is not valid anymore, " "deleting it", image.id
                 )
-                self.client.images.delete(image.id)
+                self.client.image.delete(image)
 
         LOG.info("Sync terminated for image list '%s'", image_list.name)
 
